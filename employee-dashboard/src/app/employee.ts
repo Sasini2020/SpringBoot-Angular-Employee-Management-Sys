8861388cbb888465360f1a1ea0fd8b2c80374cab
ֆǟֆɨռɨ <63692107+Sasini2020@users.noreply.github.com>
2021-07-30 20:04:18 +0530

    Updated files

---
+++ employee-dashboard/src/app/employee.ts
@@ -0,0 +1,9 @@
+//Had to add assignment beacause got a warning
+
+export class Employee {
+    id!: number;
+    firstName!: string;
+    lastName!: string;
+    emailId!: string;
+    active: boolean = false;
+}
